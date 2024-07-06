1cc7f60c8899f7ba3f9b33c1b5b591c9731bd13e
Hale Sheikhi <hale.sheikhi81@gmail.com>
2024-07-06 16:22:19 +0330

    some changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter, HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { GlobalProvider } from './context/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <GlobalProvider>
             <App />
         </GlobalProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
