ef405983769459ceab33807b6359ab057e47b568
hale <hale.new81@gmail.com>
2025-08-05 12:51:00 +0330

    added link to hero section

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,13 +1,16 @@
 import bookImg from '../assets/images/hero-img2.png';
+import {useNavigate} from "react-router";
 
 function HeroSection() {
+    const navigate = useNavigate();
+
     return (
         <div className="hero-section">
             <div className="promotion">
                 <div className="description">
                     <h1>کاراگاه درونتو بیدار کن!</h1>
                 <p>سفر به دنیای رمز آلود کتاب پرفروش <b>خدمتکار</b></p>
-                <button type='button'>مشاهده و خرید</button>
+                <button type='button' onClick={() => navigate(`/books/housemaid`)}>مشاهده و خرید</button>
                 </div>
                 <img src={bookImg} alt="book image" />
             </div>
